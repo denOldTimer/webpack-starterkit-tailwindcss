d255a94e98b3bfbb643d9895a8abc054ca5825e9
denOldTimer <kribo@outlook.com>
2020-04-13 05:56:52 +0200

    3.0.0

---
+++ readme.md
@@ -0,0 +1,13 @@
+# This is a starterkit for projects using tailwindcss
+
+### Version 3.0.0
+[2020-04-13]
+- changed the file structure (views)
+- added cssnano
+- added purgecss-webpack-plugin
+- added clean-webpack-plugin
+- added webpack-dev-server
+- added browser-sync
++ update dependecies
+    - css-loader           ^3.5.1  →  ^3.5.2 
+    - html-webpack-plugin  ^4.0.4  →  ^4.2.0 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
+const glob = require('glob');
 const autoprefixer = require("autoprefixer");
 const tailwindcss = require("tailwindcss");
 const pimport = require("postcss-import");
+const cssnano = require("cssnano");
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserJSPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 module.exports = {
+  devServer: {
+    contentBase: './public'
+  },
   //mode: "production",
   mode: "development",
   optimization: {
@@ -66,32 +78,48 @@ module.exports = {
               plugins: () => [
                 pimport(),
                 tailwindcss("tailwind.config.js"),
-                autoprefixer()
+                autoprefixer(),
+                cssnano({ preset: 'default' }),
               ]
             }
           },
           {
             loader: "sass-loader",
-            options: { plugins: () => [autoprefixer()] }
+            //options: { plugins: () => [autoprefixer()] }
           }
         ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       //options similar to same options in WebpackOptions.output
       //both options are optional
       filename: "[name].bundle.css",
       chunckFilename: "[id].css"
     }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./src/views/index.html",
       filename: "./index.html"
     }),
     new HtmlWebPackPlugin({
-      template: "./src/about.html",
+      template: "./src/views/about.html",
       filename: "./about.html"
-    })
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8080/'
+    },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: false
+      })
   ]
 };
